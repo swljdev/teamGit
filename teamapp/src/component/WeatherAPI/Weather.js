881a985e444221620fabfbea4a0407c97083c4a6
Quinn Edwards <quinnedwards@quinns-mbp.lan>
2020-03-27 12:43:18 -0400

    weather + button

--- teamapp/src/component/WeatherAPI/Weather.js
+++ teamapp/src/component/WeatherAPI/Weather.js
@@ -7,9 +7,13 @@ export default class Weather extends React.Component {
         super(props);
         this.state = {
             isLoading: false,
+            units: 'metric',
             temp: '',
-          
+            feels_like: '',
+            temp_min: '',
+            temp_max: '',
         };
+        this.handleUnitChange = this.handleUnitChange.bind(this)
     }
 
     componentDidUpdate(prevProps) {
@@ -17,18 +21,60 @@ export default class Weather extends React.Component {
         console.log(this.props.latitude)
         console.log(this.props.longitude)
               
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.latitude}&lon=${this.props.longitude}&appid=${key}`)
-            .then(res => res.json())
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.props.latitude}&lon=${this.props.longitude}&appid=${key}&units=metric`)
+            .then(response => response.json())
             .then(json => {
-                console.log(json)
+                // console.log(json.main)
+                this.setState({
+                    isLoading: false,
+                    temp: json.main.temp,
+                    feels_like: json.main.feels_like,
+                    temp_min: json.main.temp_min,
+                    temp_max: json.main.temp_max,
+
             })
+        })
             .catch(error => console.log('error', error));
-        
     }}
+
+    handleUnitChange() {
+        if (this.state.units === 'metric'){
+            let newTemp = (this.state.temp * (9/5)) + (32)
+            let newFeels = (this.state.feels_like * (9/5)) + (32)
+            let newMin = (this.state.temp_min * (9/5)) + (32)
+            let newMax = (this.state.temp_max * (9/5)) + (32)
+            this.setState({
+                temp: newTemp,
+                feels_like: newFeels,
+                temp_min: newMin,
+                temp_max: newMax,
+                units: 'imperial'
+            })
+        } else if (this.state.units === 'imperial'){
+            let newTemp = (this.state.temp - 32) *(5/9)
+            let newFeels = (this.state.feels_like - 32) *(5/9)
+            let newMin = (this.state.temp_min - 32) *(5/9)
+            let newMax = (this.state.temp_max - 32) *(5/9)
+            this.setState({
+                temp: newTemp,
+                feels_like: newFeels,
+                temp_min: newMin,
+                temp_max: newMax,
+                units: 'metric'
+            })
+        }
+    }
 
 render() {
     return (
-        <p></p>
+        <div className="weather">
+            <h4>Today's Weather</h4>
+                <li>Todays temperature is: {this.state.temp}</li>
+                <li>It feels like: {this.state.feels_like}</li>
+                <li>The maximum temperature is: {this.state.temp_max}</li>
+                <li>The minimum temperature is: {this.state.temp_min}</li>
+            <button onClick={this.handleUnitChange}>Change C/F</button>
+        </div>
     )
 }
 }
